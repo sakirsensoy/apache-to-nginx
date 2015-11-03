aa03c3cdac6e0477ae3e63ad1216b7a1097521e9
vagrant <vagrant@homestead>
2015-11-03 22:37:59 +0000

    initial commit

---
+++ default.tpl
@@ -0,0 +1,38 @@
+server {
+    listen 80;
+    server_name {serverName};
+    root {directory};
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log /var/log/nginx/{serverName}-access.log;
+    error_log  /var/log/nginx/{serverName}-error.log error;
+
+    error_page 404 /index.php;
+
+    sendfile off;
+
+    location ~ \.php$ {
+        try_files  $uri =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_read_timeout 120;
+        fastcgi_send_timeout 120;
+        fastcgi_index index.php;
+        fastcgi_param  SCRIPT_FILENAME   $document_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
---
+++ main.js
@@ -0,0 +1,51 @@
+var fs         = require('graceful-fs');
+var apacheconf = require('apacheconf');
+var format     = require('string-template');
+
+// parameters
+var args           = process.argv;
+var apacheConfFile = args[2];
+var outputFolder   = args[3];
+var tplFile        = args[4];
+
+// parameter controls
+if (! apacheConfFile || ! outputFolder) throw 'Error: Wrong parameters!';
+
+// get nginx conf template
+var configTpl = fs.readFileSync(tplFile || __dirname + '/default.tpl', 'utf8');
+
+// read apache virtual.conf
+apacheconf(apacheConfFile, function (err, config, parser) {
+
+	if (err) throw err;
+
+	config.VirtualHost.forEach(function (hostConf) {
+
+		// prepare server name
+		var serverName = [];
+
+		if (hostConf.ServerName) {
+
+			serverName.push(hostConf.ServerName[0]);
+		}
+
+		if (hostConf.ServerAlias && hostConf.ServerAlias.length > 0) {
+
+			serverName = serverName.concat(hostConf.ServerAlias);
+		}
+
+		// format template
+		var formattedTpl = format(configTpl, {
+			serverName : serverName[0],
+			directory  : hostConf.DocumentRoot[0]
+		});
+
+		// save nginx config file
+		fs.writeFile(outputFolder + '/' + serverName[0] + '.conf', formattedTpl, 'utf8', function (err) {
+
+			if (err) throw err;
+
+			console.log(serverName[0] + ' configuration saved.');
+		});
+	});
+});
